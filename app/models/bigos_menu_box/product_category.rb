dd00f6ebdd909222cf81672b4dcf3a9057d6919c
kremenso <kremenso@o2.pl>
2013-01-31 21:54:25 +0100

    relation

--- app/models/bigos_menu_box/product_category.rb
+++ app/models/bigos_menu_box/product_category.rb
@@ -1,5 +1,9 @@
 module BigosMenuBox
   class ProductCategory < ActiveRecord::Base
     attr_accessible :name, :parent_product_category_id, :position, :bigos_menu_box_element_id, :type
+    has_many :subcategories, :foreign_key => :parent_product_catagory_id, :class_name => "BigosMenuBox::ProductCategory", :dependent => :destroy
+    has_many :products, :dependent => :destroy, :class_name => "BigosMenuBox::Product"
+
+    accepts_nested_attributes_for :subcategories, :products
   end
 end
