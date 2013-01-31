a70cf96cbec4df9ac5dd89005be7c9fb239b25cd
kremenso <kremenso@o2.pl>
2013-01-31 22:04:01 +0100

    typo

--- app/models/bigos_menu_box/product_category.rb
+++ app/models/bigos_menu_box/product_category.rb
@@ -1,7 +1,7 @@
 module BigosMenuBox
   class ProductCategory < ActiveRecord::Base
     attr_accessible :name, :parent_product_category_id, :position, :bigos_menu_box_element_id, :type
-    has_many :subcategories, :foreign_key => :parent_product_catagory_id, :class_name => "BigosMenuBox::ProductCategory", :dependent => :destroy
+    has_many :subcategories, :foreign_key => :parent_product_category_id, :class_name => "BigosMenuBox::ProductCategory", :dependent => :destroy
     has_many :products, :dependent => :destroy
 
     accepts_nested_attributes_for :subcategories, :products
